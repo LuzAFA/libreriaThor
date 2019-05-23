535c3893fbf4ad2943d99d8e557b89bd6d6d36be
luz fernandez <luz.fernandez01@uptc.edu.co>
2019-05-23 16:50:19 -0500

    Update content.js

--- js/content.js
+++ js/content.js
@@ -1,22 +1,28 @@
+let fs = require('fs');
 let data = {
     "home": {
         "contenidoPagina": "<h1> este es el home</h1>",
     },
     "consulta": {
-        "contenidoPagina": '../html/plantilla.html',
-    },
-    "plantilla": {
-        "title":
-            "contenido para pagina plantilla.html q es el index  ",
-        "content1":
-            "este es el primer contenido de la otra pag ",
-        "content2":
-            "segundo contenido tbn de la segunda pag XD",
+        "contenidoPagina": function () {
+           return fs.readFileSync('../html/busqueda.html');
+        },
 
-        "img":
-            "https://vignette.wikia.nocookie.net/marioparty/images/5/5e/Estrella.png/revision/latest?cb=20121216194216&path-prefix=es"
-    },
+        "plantilla":
+            {
+                "title":
+                    "contenido para pagina plantilla.html q es el index  ",
+                "content1":
+                    "este es el primer contenido de la otra pag ",
+                "content2":
+                    "segundo contenido tbn de la segunda pag XD",
 
-};
+                "img":
+                    "https://vignette.wikia.nocookie.net/marioparty/images/5/5e/Estrella.png/revision/latest?cb=20121216194216&path-prefix=es"
+            }
+        ,
+
+    },
+}
 
 module.exports.data = data;
