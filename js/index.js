2a63776d9131551c4df3447951ec5d0782f8b531
luz fernandez <luz.fernandez01@uptc.edu.co>
2019-05-23 11:02:13 -0500

    base con css

--- js/index.js
+++ js/index.js
@@ -8,12 +8,14 @@ contenido = require('./content.js').data;
 eventos = new events();
 
 http.createServer((request, response) => {
-    contador++;
+
     let htmlContent = "";
-    fs.readFile('../html/home.html', (error, dato) => {
+    fs.readFile('../plantilla.html', (error, dato) => {
         htmlContent += dato.toString();
         let urlPath = path.basename(url.parse(request.url).pathname);
 
+
+        /// cambio de contenido
         switch (urlPath) {
             case "pag1":
                 htmlContent = remplaceContent(htmlContent, contenido.pag1);
@@ -24,20 +26,17 @@ http.createServer((request, response) => {
             case"about":
                 htmlContent = remplaceContent(htmlContent, contenido.about);
                 break;
-            case "":
-                //var output = mustache.render(template, contenido.pag1);
-                break;
         }
         response.write(htmlContent);
         response.end();
 
     });
 
-    fs.readFile('../json/comics.json', (Error, data) => {
+    fs.readFile('../dataContent/comics.json', (Error, data) => {
         let datos = JSON.parse(data);
-        console.log(datos.comics[0].link + " ");
+        //console.log(datos.comics[0].link + " ");
     })
-    
+
 }).listen(8080);
 
 eventos.once('lecJson', () => {
@@ -47,10 +46,17 @@ eventos.once('lecJson', () => {
 function remplaceContent(file, data) {
     let htmlTexto = file,
         cambios = file.match(/[^\{\{\}\}]+(?=\}\})/g);
+    console.log(cambios);
+    if (cambios != null) {
+        cambios.forEach((cambio) => {
+            htmlTexto = htmlTexto.replace(`{{${cambio}}}`, data[cambio]);
 
-    cambios.forEach((cambio) => {
-        htmlTexto = htmlTexto.replace(`{{${cambio}}}`, data[cambio]);
-    });
+        });
+    }
     return htmlTexto;
 }
+
+
+
+
 
