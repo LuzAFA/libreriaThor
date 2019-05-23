1ebb2d46b0ad4cb31fab93fbcd007f426419e653
luz fernandez <luz.fernandez01@uptc.edu.co>
2019-05-23 09:49:05 -0500

    pruebas

--- js/index.js
+++ js/index.js
@@ -1,9 +1,56 @@
-const http = require('http').createServer();
+const http = require('http'),
+    path = require('path'),
+    fs = require('fs'),
+    url = require('url'),
+    events = require('events').EventEmitter;
+contenido = require('./content.js').data;
 
-let miServidor = (req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1> Prueba desde node en git pages </h1> ')
-};
+eventos = new events();
 
-http.on('request', miServidor).listen(8080);
-console.log('conectado');
+http.createServer((request, response) => {
+    contador++;
+    let htmlContent = "";
+    fs.readFile('../html/home.html', (error, dato) => {
+        htmlContent += dato.toString();
+        let urlPath = path.basename(url.parse(request.url).pathname);
+
+        switch (urlPath) {
+            case "pag1":
+                htmlContent = remplaceContent(htmlContent, contenido.pag1);
+                break;
+            case "pag2":
+                htmlContent = remplaceContent(htmlContent, contenido.pag2);
+                break;
+            case"about":
+                htmlContent = remplaceContent(htmlContent, contenido.about);
+                break;
+            case "":
+                //var output = mustache.render(template, contenido.pag1);
+                break;
+        }
+        response.write(htmlContent);
+        response.end();
+
+    });
+
+    fs.readFile('../json/comics.json', (Error, data) => {
+        let datos = JSON.parse(data);
+        console.log(datos.comics[0].link + " ");
+    })
+    
+}).listen(8080);
+
+eventos.once('lecJson', () => {
+
+});
+
+function remplaceContent(file, data) {
+    let htmlTexto = file,
+        cambios = file.match(/[^\{\{\}\}]+(?=\}\})/g);
+
+    cambios.forEach((cambio) => {
+        htmlTexto = htmlTexto.replace(`{{${cambio}}}`, data[cambio]);
+    });
+    return htmlTexto;
+}
+
